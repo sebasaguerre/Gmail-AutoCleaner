fcde7207bf303fc2fe04cb8f61acb72c75d51544
Sebastian Aguerre <agd.sebastian@gmail.com>
2025-06-28 14:02:31 +0200

    additoinal functions and beginning shcedualer and command line arg implementation

--- email_cleaner.py
+++ email_cleaner.py
@@ -1,11 +1,13 @@
 # dependencies 
 import pickle        # serialization library ->save/load  auth. credentials
 import os
-from datetime import datetime, timedelta
+import argparse
+import schedule
 import time 
 from google.auth.transport.requests import Request # HTTP transport for google auth
 from google_auth_oauthlib.flow import InstalledAppFlow # Oauth 2.0  auth 
 from googleapiclient.discovery import bulid #  api client builder 
+from datetime import datetime, timedelta
 
 # global configuration
 # Permision levels: what are given aceess to 
@@ -56,7 +58,7 @@ class GmailCleaner:
 
         try:
             # gmial api call construction 
-            results = self.service.users().messages.list(
+            results = self.service.users().messages().list(
                 userId="me",
                 q=query,                # gmail search syntax string 
                 maxResults=max_results
@@ -70,7 +72,7 @@ class GmailCleaner:
             print(f"Error fetching emials {e}")
             return []
     
-    def delete_emails(self, message_ids, batch_size=100):
+    def delete_emails(self, message_ids, batch_size=500):
         """Delete emails in bathces"""
 
         # sanity check 
@@ -88,15 +90,14 @@ class GmailCleaner:
             batch = message_ids[i:i + batch_size]
 
             try: 
-                # iteratively delete email by email
-                for msg_id in batch:
-                    self.service.users().messages().delete(
-                        userID="me",
-                        id=msg_id
-                    ).execute()
-                    
-                    deleted += 1
+                self.service.users().messages().batchDelete(
+                    userID="me",
+                    id=batch
+                ).execute()
                 
+                deleted += len(batch)
+                time.sleep(0.1)
+            
                 print(f"Deleted {deleted/total} emails...")
 
                 # delay program to respect API limits
@@ -126,7 +127,7 @@ class GmailCleaner:
             print("No old emails found")
     
     def clean_by_sender(self, sender_email):
-        """Clean all emails from a specific sender"""
+        """Delete all emails from a specific sender"""
         query = f"from:{sender_email}"
 
         print(f"Retrieving emails from sender {sender_email}...")
@@ -139,6 +140,124 @@ class GmailCleaner:
             self.delete_emails(msg_ids)
         else:
             print("No emails found from sender")
+    
+    def clean_promotion_emails(self):
+        """Delete all promotion/marketing emials"""
+        queries = [
+            "category:promotions",
+            "unsubscribe",
+            "subject:newsletter",
+            "subject:sale",
+            "subject:discount"
+        ]
 
+        all_msg_ids = []
 
+        # loop over queries, get messages and extract ids 
+        for query in queries:
+            
+            messages = self.get_emails_by_query(query)
+            if messages:
+                msg_ids = [msg["id"] for msg in messages]
+                all_msg_ids.extend(msg_ids)
 
+        # remove duplicate ids
+        unique_ids = list(set(all_msg_ids))
+
+        # delete all emails if not empty
+        if unique_ids:
+            print(f"Found {len(unique_ids)} promotion/marketing emails...")
+            self.delete_emails(unique_ids)
+        else:
+            print("No promotion/marketing emials found")
+
+    def clean_large_emails(self, size_mb):
+        """Delete all emails above a certain size"""
+        query = f"larger:{size_mb}M"
+
+        print(f"Retriveing emails larger than {size_mb}MB...")
+        messages = self.get_emails_by_query(query)
+
+        if messages:
+            msg_ids = [msg["id"] for msg in messages]
+            print(f"Found {len(msg_ids)} large emails...")
+            self.delete_emails(msg_ids)
+
+        else:
+            print("No large emails found")
+
+    def clean_spam_emails(self):
+        """Delete all emails flagged as spam by Gmail spam detector"""
+        spam_messages = self.get_mails_by_query("in:spam")
+
+        if spam_messages:
+            msg_ids = [msg_ids["id"] for msg_ids in spam_messages]
+            self.delete_emails(msg_ids)
+        else:
+            print("No spam email found")
+    
+    def empty_trash(self):
+        """Empty the trash folder"""
+        # get all messages form the trash 
+        trash_messages = self.get_emails_by_query("in:trash")
+
+        # extract messages and delete
+        if trash_messages:
+            msg_ids = [msg["id"] for msg in trash_messages]
+
+            try:
+                self.service.users().messages.batchDelete(
+                    userId="me",
+                    body={"ids": msg_ids}
+                )
+            except Exception as e:
+                print(f"Error emptying trash: {e}")
+
+def run_cleanup(args):
+    """Run cleanup based on command line arguments"""
+
+    cleaner = GmailCleaner()
+    cleaner.authenticate()
+
+    print("Starting Gmail cleanup...")
+
+    if args.old_emails:
+        cleaner.clean_old_emails(days_old=args.old_emails)
+    
+    if args.promotional:
+        cleaner.clean_promotion_emails()
+    
+    if args.spam:
+        cleaner.clean_spam_emails()
+    
+    if args.larger_emails():
+        cleaner.clean_large_emails(size_mb=args.large_emails)
+    
+    if args.sender:
+        for sender in args.sender:
+            cleaner.clean_by_sender(sender)
+    
+    if args.empty_trash:
+        cleaner.empty_trash()
+
+    print("Cleanup finalzed!")
+
+def run_schedualed_cleanup():
+    """Run default cleanup for scheduled execution"""
+
+    # Default cleanup 
+    cleaner = GmailCleaner()
+    cleaner.authenticate()
+
+    # default cleanup
+    cleaner.clean_promotion_emails()
+    cleaner.clean_spam_emails()
+
+    print("Schedualed cleanup completed!")
+
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    main()
